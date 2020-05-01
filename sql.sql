25c8e53ab783fa20c108477a11e7a71ab42d2d2b
Tyler van der Hoeven <hi@tyvdh.com>
2020-05-01 10:26:55 -0400

    updates

--- sql.sql
+++ sql.sql
@@ -8,14 +8,14 @@ CREATE TABLE contracts(
 	contract VARCHAR(56) PRIMARY KEY UNIQUE NOT NULL,
 	signer VARCHAR(56) UNIQUE NOT NULL,
 	pendingTxns VARCHAR(64)[] DEFAULT NULL,
-	nextDedupe BIGINT DEFAULT (extract(epoch from now()) * 1000 + 60000) NOT NULL, -- In 1 minute
-	nextFlush BIGINT DEFAULT (extract(epoch from now()) * 1000 + 3600000) NOT NULL -- In 1 hour
+	nextDedupe BIGINT DEFAULT (extract(epoch from now()) * 1000) NOT NULL,
+	nextFlush BIGINT DEFAULT (extract(epoch from now()) * 1000) NOT NULL
 )
 
 ALTER TABLE contracts
 ADD COLUMN pendingTxns VARCHAR(64)[] DEFAULT NULL,
-ADD COLUMN nextDedupe BIGINT DEFAULT (extract(epoch from now()) * 1000 + 60000) NOT NULL, -- In 1 minute
-ADD COLUMN nextFlush BIGINT DEFAULT (extract(epoch from now()) * 1000 + 3600000) NOT NULL -- In 1 hour
+ADD COLUMN nextDedupe BIGINT DEFAULT (extract(epoch from now()) * 1000) NOT NULL,
+ADD COLUMN nextFlush BIGINT DEFAULT (extract(epoch from now()) * 1000) NOT NULL
 
 INSERT INTO contracts (contract, signer)
 select 'GCNANNNLGDICM5NJOT7QD7MLW34M4WLJPTNOAXWEAR4CE4LO23FZ5WDR', 'SDRJWX2SNJTMFFGGGO4DQXL7X5SHVENZLYX6K7LCZQSIJ6XSEMTCXLHY'
