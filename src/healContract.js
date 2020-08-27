f084ee921a7ec0922b2c029f443cc4b329aecc8a
Tyler van der Hoeven <hi@tyvdh.com>
2020-08-27 12:46:45 -0400

    remove old data add new

--- src/healContract.js
+++ src/healContract.js
@@ -8,6 +8,7 @@ import Pool from './js/pg'
 
 // TODO: Ensure a 1:1 swap, if multiple turrets are dead enforce swapping 1 at a time
   // If signer is live but not included in X-Turrets a swap is possible
+  // Remove old manage data, replace with new manage data
 
 // // GAVDUDMKRMGDF57IXX745EWKM4UPC3X7LGO6C5NKT4CDQW7MT7L6UXNR // User
 
@@ -107,6 +108,14 @@ export default async (event, context) => {
         ed25519PublicKey: addSigner.signer,
         weight: removeSigner.weight
       }
+    }))
+    .addOperation(Operation.manageData({
+      name: `TSS_${removeSigner.turret}`,
+      value: null,
+    }))
+    .addOperation(Operation.manageData({
+      name: `TSS_${addSigner.turret}`,
+      value: addSigner.signer,
     }))
     .setTimeout(0)
     .build())
